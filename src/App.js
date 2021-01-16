15a48b3be196e2b9aa8a5b6e76d61e9a385ab867
MrUnluckyy <Justas.Sobutas@gmail.com>
2021-01-16 18:59:29 +0200

    Sign in form created - no functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInUpPage from './pages/sign-in-up-page/sign-in-up-page.component';
 import Header from './components/header/header.component';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-up-page/sign-in-up-page.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-up-page.styles.scss';
+
+const SignInUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
+)
+
+export default SignInUpPage;
