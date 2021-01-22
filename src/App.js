f1ff34171b234087e4423dbb8b2eae13ead2a627
MrUnluckyy <Justas.Sobutas@gmail.com>
2021-01-22 18:38:26 +0200

    Modified sign in with google button

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null
 
-  componentDidMount() {
+componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({ currentUser: user});
       console.log(user);
@@ -34,7 +34,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -49,11 +49,12 @@ class SignIn extends React.Component {
                         handleChange={this.handleChange}
                         required
                     />
-
-                    <CustomButton type="submit"> Sign In </CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>
-                         Sign In WIth Google 
-                    </CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type="submit"> Sign In </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Sign In WIth Google 
+                        </CustomButton>
+                    </div>
                 </form>
             </div>
         )
