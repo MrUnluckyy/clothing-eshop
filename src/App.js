4787eef7b405d6b98237d60bfeacf2dd70989a0f
MrUnluckyy <Justas.Sobutas@gmail.com>
2021-01-24 14:14:29 +0200

    sign-up with email and password

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ componentDidMount() {
               ...snapShot.data()
             }
           });
+          console.log(this.state);
         });
       } else {
         this.setState({currentUser: userAuth})
