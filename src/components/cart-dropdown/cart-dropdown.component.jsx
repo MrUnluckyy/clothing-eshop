11411c0ec85c6ddceeb11246d6abe6d54c15edac
MrUnluckyy <Justas.Sobutas@gmail.com>
2021-01-28 20:57:24 +0200

    Cart qty count with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems }) => (
@@ -21,8 +23,8 @@ const CartDropdown = ({ cartItems }) => (
     </div>
 );
 
-const mapStateProps = ({ cart: { cartItems } }) => ({
-    cartItems
+const mapStateProps = state => ({
+    cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateProps)(CartDropdown);
